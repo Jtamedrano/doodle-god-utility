0dc08fbc6003846c67c8324234070323993b2448
Jesse Medrano <Jtamedrano@gmail.com>
2020-10-21 18:42:06 -0700

    Login Page Made

--- FRONTEND/src/Componenets/auth/Login.js
+++ FRONTEND/src/Componenets/auth/Login.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Form } from "react-bootstrap";
+import UserContext from "../../context/userContext";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const { setUserData } = useContext(UserContext);
+
+  const history = useHistory();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const loginRes = await Axios.post("http://localhost:5000/user/login", {
+      email,
+      password,
+    });
+    setUserData({
+      token: loginRes.data.token,
+      user: loginRes.data.user,
+    });
+    localStorage.setItem("auth-token", loginRes.data.token);
+    history.push("/");
+  };
+
   return (
     <>
-      <div>Login</div>
+      <h2>Log In</h2>
+      <Form onSubmit={submit}>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Email"
+            autoComplete="new-username"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="new-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Control type="submit" value="Login" />
+      </Form>
     </>
   );
 }
