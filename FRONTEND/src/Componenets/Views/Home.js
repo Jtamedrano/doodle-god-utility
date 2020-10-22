2fe20d56cb6637ad718b779ff70b3789093bc1fe
Jesse Medrano <Jtamedrano@gmail.com>
2020-10-21 18:41:35 -0700

    Welcome Page

--- FRONTEND/src/Componenets/Views/Home.js
+++ FRONTEND/src/Componenets/Views/Home.js
@@ -3,7 +3,33 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div>Home</div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQPrRzUbUmfjVWVbUze119yFY-URdUXPVkEWw&usqp=CAU"
+        alt="Doodle God Logo's"
+      />
+      <h2>Welcome to DGU</h2>
+      <p>
+        The Doodle God Mobile App Series is a puzzle game where users start with
+        the 4 original elements known to man and create other elements by
+        combining those elements together.
+      </p>
+      <p>
+        Trying to remember what combinations make what is one part of the game
+        but the other part is remembering what didn't work so you can save time
+        making new creations. Doodle God Utility helps track all possible
+        combinations that haven't been made yet and helps the user keep track of
+        past combinations.
+      </p>
+      <p>
+        An account is used to keep track of these changes to each user. The user
+        can store what game they are playing, what level one is on, and track
+        groups and elements unlocked, all while the app calculates all the
+        possible combinations.
+      </p>
+      <h4>
+        Download the Game for free on iOS or Android and Register Now to save
+        your progress
+      </h4>
     </>
   );
 }
