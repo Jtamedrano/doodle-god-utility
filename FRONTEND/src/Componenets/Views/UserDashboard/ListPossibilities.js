9a7ab6f42d9f4d881bcf532de67fd0b2b0e73b4f
Jesse Medrano <Jtamedrano@gmail.com>
2020-11-05 09:17:45 -0800

    ListPossibilites added to Dashboard

---
+++ FRONTEND/src/Componenets/Views/UserDashboard/ListPossibilities.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import Element from "../../Items/Element";
+import shortid from "shortid";
+
+export default function ListPossibilities(props) {
+  const possibilityListSet = new Set();
+  props.items.forEach((item1) => {
+    props.items.forEach((item2) => {
+      possibilityListSet.add([item1.id, item2.id].sort().toLocaleString());
+    });
+  });
+
+  const possibilityList = Array.from(possibilityListSet, (x) => x.split(","));
+
+  function findItem(e, index) {
+    return props.items.find((element) => element.id === e[index]);
+  }
+
+  return (
+    <div>
+      <h2>Possibilities</h2>
+      {possibilityList.map((e) => {
+        const elementOne = findItem(e, 0);
+        const elementTwo = findItem(e, 1);
+        return (
+          <Button
+            key={shortid.generate()}
+            className="mr-2 mt-2"
+            value={`${elementOne.id}-${elementTwo.id}`}
+          >
+            <Element name={elementOne.name} /> -{" "}
+            <Element name={elementTwo.name} />
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
